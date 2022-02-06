7767cb6879c379e4cfd2ad40164a8569013b9083
John Stephenson <johnnyfleet@gmail.com>
2022-02-06 22:42:58 +1300

    Added blinking LED ot tell when in light and deep sleep

--- Deep_Sleep/src/main.cpp
+++ Deep_Sleep/src/main.cpp
@@ -1,17 +1,26 @@
 #include <Arduino.h> 
 
 #define uS_TO_S_FACTOR 1000000  /* Conversion factor for micro seconds to seconds */
-#define TIME_TO_SLEEP  5        /* Time ESP32 will go to sleep (in seconds) */
+#define TIME_TO_SLEEP  10        /* Time ESP32 will go to sleep (in seconds) */
+#define LED 2
 
 void setup(){
   Serial.begin(115200);
+  pinMode(LED, OUTPUT);
 
+  digitalWrite(LED, HIGH);
+  Serial.println("LED is on");
   esp_sleep_enable_timer_wakeup(TIME_TO_SLEEP * uS_TO_S_FACTOR);
   Serial.println("Setup ESP32 to sleep for every " + String(TIME_TO_SLEEP) + " Seconds");
 
+  delay(10000); /* stay on for 5 seconds */
+  
+  digitalWrite(LED, LOW);
+  Serial.println("LED is off");
+
   Serial.println("Going to sleep now");
-  delay(1000);
   Serial.flush(); 
+  
   esp_deep_sleep_start();
   Serial.println("This message will never be printed");
 }
